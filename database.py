cd1a145821ba7b0bbe8cc5c395f1c3b0d6c9d95b
Luis Alonso <luisalonso97@gmail.com>
2025-12-19 12:05:57 -0600

    Added ping b4 conn to avoid 500 on 1st req

--- database.py
+++ database.py
@@ -2,7 +2,13 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from config import settings
 
-engine = create_async_engine(settings.database_url, echo=False, future=True)
+engine = create_async_engine(
+    settings.database_url,
+    echo=False,
+    future=True,
+    pool_pre_ping=True,
+    pool_recycle=1800,
+)
 
 AsyncSessionLocal = sessionmaker(
     engine,
